2c8d95f6843cf477d172daccb99f4de0e083fd21
Rustam Distanov <logika06@yandex.ru>
2025-02-18 23:50:01 +0300

    fix BaseTest class and delete BrowserFactory class

--- src/test/java/pages/BaseTest.java
+++ src/test/java/pages/BaseTest.java
@@ -1,19 +1,27 @@
 package pages;
 
-
-import browser.BrowserFactory;
 import org.junit.Before;
 
 public class BaseTest {
-    private BrowserFactory browser = new BrowserFactory();
 
     @Before
     public void browserSetup() {
         /*
-        для выбора класса укажите значение yandex или chrome
+        Теперь запустить тесты в яндекс браузере можно командой mvn clean test -Dbrowser=yandex
+        Без команды -Dbrowser=yandex тесты будут прогоняться в хроме
          */
-        browser.setBrowser("chrome");
+
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
+
+        if ("yandex".equals(browser)) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/yandexdriver.exe");
+            // При необходимости скорректировать путь до Яндекс браузера
+            System.setProperty("webdriver.chrome.binary", "C:/Users/User/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/chromedriver.exe");
+            System.setProperty("webdriver.chrome.binary", "C:/Program Files/Google/Chrome/Application/chrome.exe");
+        }
+
 
     }
-
 }
