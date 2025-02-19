b072019ff9ee41ca78ba76365bcdaf835e280920
Rustam Distanov <logika06@yandex.ru>
2025-02-19 22:57:06 +0300

    delete absolute path

--- src/test/java/pages/BaseTest.java
+++ src/test/java/pages/BaseTest.java
@@ -6,6 +6,7 @@ public class BaseTest {
 
     @Before
     public void browserSetup() {
+
         /*
         Теперь запустить тесты в яндекс браузере можно командой mvn clean test -Dbrowser=yandex
         Без команды -Dbrowser=yandex тесты будут прогоняться в хроме
@@ -15,11 +16,8 @@ public class BaseTest {
 
         if ("yandex".equals(browser)) {
             System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/yandexdriver.exe");
-            // При необходимости скорректировать путь до Яндекс браузера
-            System.setProperty("webdriver.chrome.binary", "C:/Users/User/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
         } else {
             System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/chromedriver.exe");
-            System.setProperty("webdriver.chrome.binary", "C:/Program Files/Google/Chrome/Application/chrome.exe");
         }
 
 
